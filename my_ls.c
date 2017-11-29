84b5910283da5f08b8162e2b54183c81d93509bc
Vincent Attali <vattali@localhost.localdomain>
2017-11-29 14:06:34 +0100

    "fixed malloc's"

--- my_ls.c
+++ my_ls.c
@@ -12,26 +12,11 @@
 #include <stdlib.h>
 #include "include/my.h"
 
-int check_error(char **av, char **files, int i, int j)
-{
-	struct stat s;
-
-	if (stat(av[i], &s) == -1) {
-		write(2, "ls: cannot access '", 19);
-		write(2, av[i], my_strlen(av[i]));
-		write(2, "': No such file or directory\n", 29);
-		return (1);
-	} else {
-		files[j] = malloc(sizeof(char) * 255);
-		my_strcpy(files[j], av[i]);
-	}
-	return (0);
-}
-
 int main(int ac, char **av)
 {
-	char **files = malloc(sizeof(char*) * ac - 1);
-	char **folders = malloc(sizeof(char*) * ac - 1);
+	char **files = malloc(sizeof(char*) * (ac - 1));
+	char **folders = malloc(sizeof(char*) * (ac - 1));
+	struct stat s;
 	struct dirent *file_read = NULL;
 	DIR* rep = NULL;
 	int error = 0;
@@ -40,31 +25,40 @@ int main(int ac, char **av)
 
 	for (int i = 1; i < ac; i++) {
 		if (opendir(av[i]) == NULL) {
-			if (check_error(av, files, i, j) == 1)
+			if (stat(av[i], &s) == -1) {
+				write(2, "ls: cannot access '", 19);
+				write(2, av[i], my_strlen(av[i]));
+				write(2, "': No such file or directory\n", 29);
 				error = 1;
-			else
+			} else {
+				files[j] = malloc(sizeof(char) * my_strlen(av[i]) + 1);
+				my_strcpy(files[j], av[i]);
 				j++;
+			}
 		} else {
-			folders[k] = malloc(sizeof(char) * 255);
+			folders[k] = malloc(sizeof(char) * my_strlen(av[i]) + 1);
 			my_strcpy(folders[k], av[i]);
 			k++;
 		}
 	}
 	for (int i = 0; i < j; i++) {
 		my_putstr(files[i]);
-		if (i < j - 1)
-			my_putchar('\n');
+		my_putchar('\n');
 		free(files[i]);
 	}
 	for (int i = 0; i < k; i++) {
-		my_putstr("\n\n");
 		my_putstr(folders[i]);
 		my_putstr(":\n");
 		rep = opendir(folders[i]);
-	        if ((file_read = readdir(rep)) != NULL)
-			my_putstr(file_read->d_name);
+		while ((file_read = readdir(rep)) != NULL) {
+			if (file_read->d_name[0] != '.') {
+				my_putstr(file_read->d_name);
+				my_putchar('\n');
+			}
+		}
+		closedir(rep);
 		if (i < k - 1)
-			my_putchar(' ');
+			my_putchar('\n');
 		free(folders[i]);
 	}
 	free(files);
