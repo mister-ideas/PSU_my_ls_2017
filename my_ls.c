29f6d79daab39ae7bc8228621ae083724cddc296
Vincent Attali <vattali@localhost.localdomain>
2017-11-29 11:40:44 +0100

    "added files & folders handling"

--- my_ls.c
+++ my_ls.c
@@ -5,42 +5,71 @@
 ** my_ls.c
 */
 
+#include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include "include/my.h"
 
-int main(int ac, char **av)
+int check_error(char **av, char **files, int i, int j)
 {
 	struct stat s;
+
+	if (stat(av[i], &s) == -1) {
+		write(2, "ls: cannot access '", 19);
+		write(2, av[i], my_strlen(av[i]));
+		write(2, "': No such file or directory\n", 29);
+		return (1);
+	} else {
+		files[j] = malloc(sizeof(char) * 255);
+		my_strcpy(files[j], av[i]);
+	}
+	return (0);
+}
+
+int main(int ac, char **av)
+{
 	char **files = malloc(sizeof(char*) * ac - 1);
+	char **folders = malloc(sizeof(char*) * ac - 1);
+	struct dirent *file_read = NULL;
+	DIR* rep = NULL;
 	int error = 0;
 	int j = 0;
 	int k = 0;
 
 	for (int i = 1; i < ac; i++) {
-		if (stat(av[i], &s) == -1) {
-			write(2, "ls: cannot access '", 19); 
-			write(2, av[i], my_strlen(av[i]));
-			write(2, "': No such file or directory\n", 29);
-			error = 1;
+		if (opendir(av[i]) == NULL) {
+			if (check_error(av, files, i, j) == 1)
+				error = 1;
+			else
+				j++;
 		} else {
-			files[j] = malloc(sizeof(char) * 255);
-			my_strcpy(files[j], av[i]);
-			j++;
+			folders[k] = malloc(sizeof(char) * 255);
+			my_strcpy(folders[k], av[i]);
 			k++;
 		}
 	}
-	for (int i = 0; i < k; i++) {
+	for (int i = 0; i < j; i++) {
 		my_putstr(files[i]);
-		if (i < k - 1)
-			my_putchar(' ');
+		if (i < j - 1)
+			my_putchar('\n');
 		free(files[i]);
 	}
+	for (int i = 0; i < k; i++) {
+		my_putstr("\n\n");
+		my_putstr(folders[i]);
+		my_putstr(":\n");
+		rep = opendir(folders[i]);
+	        if ((file_read = readdir(rep)) != NULL)
+			my_putstr(file_read->d_name);
+		if (i < k - 1)
+			my_putchar(' ');
+		free(folders[i]);
+	}
 	free(files);
+	free(folders);
 	if (error == 1)
 		return (84);
-	else
-		return (0);
+	return (0);
 }
