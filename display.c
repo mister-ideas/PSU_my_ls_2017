5785c296a2dc6000a8f5c6c558185ed6c145dd64
Vincent Attali <vattali@linux-2.home>
2017-12-03 14:45:21 +0100

    "fixed norm errors"

--- display.c
+++ display.c
@@ -16,20 +16,6 @@
 #include "my.h"
 #include "my_ls.h"
 
-void print_rights(struct stat s)
-{
-	((s.st_mode & S_IFDIR) != 0) ? my_putchar('d') : my_putchar('-');
-	((s.st_mode & S_IRUSR) != 0) ? my_putchar('r') : my_putchar('-');
-	((s.st_mode & S_IWUSR) != 0) ? my_putchar('w') : my_putchar('-');
-	((s.st_mode & S_IXUSR) != 0) ? my_putchar('x') : my_putchar('-');
-	((s.st_mode & S_IRGRP) != 0) ? my_putchar('r') : my_putchar('-');
-	((s.st_mode & S_IWGRP) != 0) ? my_putchar('w') : my_putchar('-');
-	((s.st_mode & S_IXGRP) != 0) ? my_putchar('x') : my_putchar('-');
-	((s.st_mode & S_IROTH) != 0) ? my_putchar('r') : my_putchar('-');
-	((s.st_mode & S_IWOTH) != 0) ? my_putchar('w') : my_putchar('-');
-	((s.st_mode & S_IXOTH) != 0) ? my_putchar('x') : my_putchar('-');
-}
-
 void print_folder_files(char *folder)
 {
 	DIR *rep = NULL;
@@ -48,12 +34,10 @@ void print_folder_files(char *folder)
 void l_print_folder_files(char *folder)
 {
 	DIR *rep = NULL;
+	char path[255];
 	struct dirent *file;
-	struct stat s;
 	struct passwd *passwd;
 	struct group *group;
-	char path[255];
-	char *mtime;
 
 	if (folder[my_strlen(folder) - 1] != '/')
 		my_strcat(folder, "/");
@@ -64,23 +48,7 @@ void l_print_folder_files(char *folder)
 		if (file->d_name[0] != '.') {
 			my_strcpy(path, folder);
 			my_strcat(path, file->d_name);
-			stat(path, &s);
-			print_rights(s);
-			my_putstr(". ");
-			my_put_nbr(s.st_nlink);
-			my_putchar(' ');
-			passwd = getpwuid(s.st_uid);
-			my_putstr(passwd->pw_name);
-			my_putchar(' ');
-			group = getgrgid(s.st_gid);
-			my_putstr(group->gr_name);
-			my_putchar(' ');
-			my_put_nbr(s.st_size);
-			my_putchar(' ');
-			mtime = ctime(&(s.st_mtim.tv_sec));
-			write(1, mtime + 4, 12);
-			my_putchar(' ');
-			my_putstr(file->d_name);
+			l_display(passwd, group, path, file->d_name);
 			my_putchar('\n');
 		}
 	}
@@ -111,30 +79,12 @@ void no_flag_display(char **files, char **folders, int j, int k)
 
 void l_flag_display(char **files, char **folders, int j, int k)
 {
-	struct stat s;
 	struct passwd *passwd;
 	struct group *group;
-	char *mtime;
 
 	for (int i = 0; i < j; i++) {
 		if (files[i][0] != '$') {
-			stat(files[i], &s);
-			print_rights(s);
-			my_putstr(". ");
-			my_put_nbr(s.st_nlink);
-			my_putchar(' ');
-			passwd = getpwuid(s.st_uid);
-			my_putstr(passwd->pw_name);
-			my_putchar(' ');
-			group = getgrgid(s.st_gid);
-			my_putstr(group->gr_name);
-			my_putchar(' ');
-			my_put_nbr(s.st_size);
-			my_putchar(' ');
-			mtime = ctime(&(s.st_mtim.tv_sec));
-			write(1, mtime + 4, 12);
-			my_putchar(' ');
-			my_putstr(files[i]);
+			l_display(passwd, group, files[i], files[i]);
 			my_putchar('\n');
 		}
 	}
--- tools.c
+++ tools.c
@@ -5,7 +5,14 @@
 ** tools.c
 */
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 #include "my.h"
 #include "my_ls.h"
 
@@ -22,4 +29,45 @@ void free_list(char **list, int nb)
 	for (int i = 0; i < nb; i++)
 		free(list[i]);
 	free(list);
+}
+
+void print_rights(struct stat s)
+{
+	((s.st_mode & S_IFDIR) != 0) ? my_putchar('d') : my_putchar('-');
+	((s.st_mode & S_IRUSR) != 0) ? my_putchar('r') : my_putchar('-');
+	((s.st_mode & S_IWUSR) != 0) ? my_putchar('w') : my_putchar('-');
+	((s.st_mode & S_IXUSR) != 0) ? my_putchar('x') : my_putchar('-');
+	((s.st_mode & S_IRGRP) != 0) ? my_putchar('r') : my_putchar('-');
+	((s.st_mode & S_IWGRP) != 0) ? my_putchar('w') : my_putchar('-');
+	((s.st_mode & S_IXGRP) != 0) ? my_putchar('x') : my_putchar('-');
+	((s.st_mode & S_IROTH) != 0) ? my_putchar('r') : my_putchar('-');
+	((s.st_mode & S_IWOTH) != 0) ? my_putchar('w') : my_putchar('-');
+	((s.st_mode & S_IXOTH) != 0) ? my_putchar('x') : my_putchar('-');
+}
+
+void l_display(struct passwd *passwd,
+	       struct group *group,
+	       char *path,
+	       char *file)
+{
+	struct stat s;
+	char *mtime;
+
+	stat(path, &s);
+	print_rights(s);
+	my_putstr(". ");
+	my_put_nbr(s.st_nlink);
+	my_putchar(' ');
+	passwd = getpwuid(s.st_uid);
+	my_putstr(passwd->pw_name);
+	my_putchar(' ');
+	group = getgrgid(s.st_gid);
+	my_putstr(group->gr_name);
+	my_putchar(' ');
+	my_put_nbr(s.st_size);
+	my_putchar(' ');
+	mtime = ctime(&(s.st_mtim.tv_sec));
+	write(1, mtime + 4, 12);
+	my_putchar(' ');
+	my_putstr(file);
 }
